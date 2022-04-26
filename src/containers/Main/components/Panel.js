60c6cffbedbd3e58c332d434ecd873914bbf193b
dyegokleodon <dyego.kleodon@gmail.com>
2022-04-26 13:00:00 -0300

    init Panel

--- src/containers/Main/components/Panel.js
+++ src/containers/Main/components/Panel.js
@@ -1,4 +1,4 @@
-import RefreshIcon from '../../../assets/images/refresh.svg'
+//import RefreshIcon from '../../../assets/images/refresh.svg'
 import {Card, Typography, Button, Select, MenuItem} from '../../../components'
 import COUNTRIES from '../../../commons/constants/countries'
 import {CardPanelContentStyled, Itemstyled} from './style'
@@ -6,7 +6,7 @@ import {CardPanelContentStyled, Itemstyled} from './style'
 const navigatorHasShare = navigator.share
 
 function Panel({updateAt, onChange, country, data, getCovidData}) {
-  const {cases, recovered, deaths, todayCases, todayDeaths} = data
+  const {recovered} = data
   
   const renderCountries = (country, index) => (
     <MenuItem key={`country.${index}`} value={country.value}>
